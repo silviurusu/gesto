3523fd2ead079e6551a02948a1def35a31d31c06
Vlad <vladisac@gmail.com>
2012-05-27 16:02:31 +0300

    grafic stoc zero

--- static/js/grafice.stocuri.js
+++ static/js/grafice.stocuri.js
@@ -1,14 +1,15 @@
-var margin = {top: 30, right: 10, bottom: 10, left: 30},
-    width = 960 - margin.right - margin.left,
-    height = 930 - margin.top - margin.bottom;
+var margin = {top: 30, right: 10, bottom: 10, left: 150},
+    width = 360 - margin.right - margin.left,
+    height = 330 - margin.top - margin.bottom;
 
-var format = d3.format(",.0f");
+var format = d3.format(",.0f"),
+    formatTime = d3.time.format("%H:%M");
 
 var x = d3.scale.linear()
     .range([0, width]);
 
 var y = d3.scale.ordinal()
-    .rangeRoundBands([0, height], .1);
+    .rangeRoundBands([0, height], .3);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -20,20 +21,23 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickSize(0);
 
-var svg = d3.select("zeroStoc-chart").append("svg")
+var svg = d3.select(".zeroStoc-chart").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("http://localhost:8000/static/js/sample-data.csv", function(data) {
+d3.csv("http://localhost:8000/static/js/stoczero.json", function(data) {
 
     // Parse numbers, and sort by value.
-    data.forEach(function(d) { d.value = +d.value; });
-    data.sort(function(a, b) { return b.value - a.value; });
+    data.forEach(function(d) {
+        d.value = parseDate(d.value) ;
+        d.name = d.name.substring(0,15);
+    });
+    data.sort(function(a, b) { return a.value - b.value; });
 
     // Set the scale domain.
-    x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    x.domain([6, 22]);
     y.domain(data.map(function(d) { return d.name; }));
 
     var bar = svg.selectAll("g.bar")
@@ -43,17 +47,17 @@ d3.csv("http://localhost:8000/static/js/sample-data.csv", function(data) {
         .attr("transform", function(d) { return "translate(0," + y(d.name) + ")"; });
 
     bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
+        .attr("width", function(d) { return x(d.value.getHours()); })
         .attr("height", y.rangeBand());
 
     bar.append("text")
         .attr("class", "value")
-        .attr("x", function(d) { return x(d.value); })
+        .attr("x", function(d) { return x(d.value.getHours()); })
         .attr("y", y.rangeBand() / 2)
         .attr("dx", -3)
         .attr("dy", ".35em")
         .attr("text-anchor", "end")
-        .text(function(d) { return format(d.value); });
+        .text(function(d) { return formatTime(d.value); });
 
     svg.append("g")
         .attr("class", "x axis")
@@ -63,3 +67,12 @@ d3.csv("http://localhost:8000/static/js/sample-data.csv", function(data) {
         .attr("class", "y axis")
         .call(yAxis);
 });
+
+// Like d3.time.format, but faster.
+function parseDate(d) {
+    return new Date(2011,
+        d.substring(0, 2) - 1,
+        d.substring(2, 4),
+        d.substring(4, 6),
+        d.substring(6, 8));
+}
