a775a9d1705870e90393454f31a7d4edced0bd2d
Vlad <vladisac@gmail.com>
2012-06-21 18:25:07 +0300

    forgot

--- static/js/pie-chart.js
+++ static/js/pie-chart.js
@@ -2,12 +2,19 @@ var width = 200,
     height = 200,
     outerRadius = Math.min(width, height) / 2,
     innerRadius = 0,
-    data = [{"label":"Cafea", "value":0.35},
+    data1 = [{"label":"Cafea", "value":0.35},
         {"label":"Panificatie", "value":0.25},
         {"label":"Patiserie", "value":0.15},
         {"label":"Paste", "value":0.1},
         {"label":"Cofetarie", "value":0.8},
-        {"label":"Ambalate", "value":0.7}];
+        {"label":"Ambalate", "value":0.7}],
+    data2 = [{"label":"Cafea", "value":0.15},
+        {"label":"Panificatie", "value":0.45},
+        {"label":"Patiserie", "value":0.05},
+        {"label":"Paste", "value":0.2},
+        {"label":"Cofetarie", "value":0.9},
+        {"label":"Ambalate", "value":0.6}],
+    data = data1,
     color = d3.scale.category20(),
     donut = d3.layout.pie().value(function(d) { return d.value; }),
     arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
@@ -41,8 +48,25 @@ arcs.append("text")
     .style("fill", "White")
     .text(function(d, i) { return data[i].label; });
 
+d3.select('#pie-chart').on("click", function() {
+    data = data === data1 ? data2 : data1; // swap the data
+    arcs = arcs.data(donut(data)); // recompute the angles and rebind the data
+//    arcs.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
+    arcs.attr("d", arc); // redraw the arcs
+});
+
+
 // Computes the angle of an arc, converting from radians to degrees.
 function angle(d) {
     var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
     return a > 90 ? a - 180 : a;
+}
+// Store the currently-displayed angles in this._current.
+// Then, interpolate from this._current to the new angles.
+function arcTween(a) {
+    var i = d3.interpolate(this._current, a);
+    this._current = i(0);
+    return function(t) {
+        return arc(i(t));
+    };
 }
