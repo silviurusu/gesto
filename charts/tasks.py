7655d527cd817ebd4227730d1c0eec9f4eade850
Vlad <vladisac@gmail.com>
2012-07-13 11:39:42 +0300

    add test task

--- charts/tasks.py
+++ charts/tasks.py
@@ -1,5 +1,11 @@
 from celery.task import task
+from celery.utils.log import get_task_logger
+from django.utils.timezone import now
+
+logger = get_task_logger(__name__)
+
 
 @task
 def add(x, y):
+    logger.info('%s:::Adding %s + %s' % (now(), x, y))
     return x + y
