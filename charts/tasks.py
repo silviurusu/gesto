2f0971e4886ace19e091db364f19492ea5c169fd
Vlad <vladisac@gmail.com>
2012-07-15 18:17:29 +0300

    move task import sales from views to tasks

--- charts/tasks.py
+++ charts/tasks.py
@@ -1,12 +1,62 @@
 from celery.utils.log import get_task_logger
-import celery
+from django.core.files.move import file_move_safe
 from django.utils.timezone import now
+from datetime import datetime
+from models import *
+from settings import CSV_PATH
+import celery
+import csv
+import os
+
+saleFieldNames = ['code','name','dep','qty','price']
 
 logger = get_task_logger(__name__)
 
+#import *.sale files from csv_path
+#filename : locAALLZZOOMM.sale
+#sale csv format is like saleFieldNames
+@celery.task()
+def csv_to_sales(request):
+    files = os.listdir(CSV_PATH)
+    for file in files:
+        filePath = os.path.join(CSV_PATH, file)
+        if os.path.isfile(filePath) and file.endswith("sale"):
+
+            fileDate = datetime.strptime( file[3:13], '%y%m%d%H%M')
+            operationType = OperationType.objects.get(name = 'sale')
+            gestiune, created = Gestiune.objects.get_or_create(name = file[:3])
+            sale = Operation.objects.create(type = operationType,
+                gestiune = gestiune,
+                operation_at = fileDate)
+
+            with open(filePath) as f:
+            #                dataReader = csv.reader(f, delimiter=',', quotechar='"')
+                dataReader = csv.DictReader(f, fieldnames=saleFieldNames, delimiter=',', quotechar='"')
+                for row in dataReader:
+
+                    saleItem = OperationItems()
+                    dep, created = Category.objects.get_or_create(name = row['dep'])
+                    product, created = Product.objects.get_or_create( code = row['code'], name = row['name'], dep = dep )
+
+                    saleItem.operation = sale
+                    saleItem.product = product
+                    saleItem.qty = row['qty']
+                    saleItem.price = row['price']
+
+                    saleItem.save()
+
+            moveToPath = os.path.join(CSV_PATH, file[0:3], file[3:5], file[5:7], file[7:9])
+            if not os.path.exists(moveToPath):
+                os.makedirs(moveToPath)
+            #TODO:handle existing file
+            file_move_safe(filePath,  moveToPath + '/' + file)
+
+    #return 'import'
+
+
 @celery.task()
 def add(x, y):
-    #logger = add.get_logger(logfile="thefootask.log")
     logger.info('%s:::Adding %s + %s' % (now(), x, y))
-    print('ll000000000000000000000ging=====================================================')
     return x + y
+
+
--- charts/views.py
+++ charts/views.py
@@ -11,48 +11,6 @@ import os
 #csv_path = 'd:/Dropbox/Shares/Prospero700/'
 from settings import CSV_PATH
 
-saleFieldNames = ['code','name','dep','qty','price']
-
-#import *.sale files from csv_path
-#filename : locAALLZZOOMM.sale
-#sale csv format is like saleFieldNames
-
-def csv_to_sales(request):
-    files = os.listdir(CSV_PATH)
-    for file in files:
-        filePath = os.path.join(CSV_PATH, file)
-        if os.path.isfile(filePath) and file.endswith("sale"):
-
-            fileDate = datetime.strptime( file[3:13], '%y%m%d%H%M')
-            operationType = OperationType.objects.get(name = 'sale')
-            gestiune, created = Gestiune.objects.get_or_create(name = file[:3])
-            sale = Operation.objects.create(type = operationType,
-                                            gestiune = gestiune,
-                                            operation_at = fileDate)
-
-            with open(filePath) as f:
-#                dataReader = csv.reader(f, delimiter=',', quotechar='"')
-                dataReader = csv.DictReader(f, fieldnames=saleFieldNames, delimiter=',', quotechar='"')
-                for row in dataReader:
-
-                    saleItem = OperationItems()
-                    dep, created = Category.objects.get_or_create(name = row['dep'])
-                    product, created = Product.objects.get_or_create( code = row['code'], name = row['name'], dep = dep )
-
-                    saleItem.operation = sale
-                    saleItem.product = product
-                    saleItem.qty = row['qty']
-                    saleItem.price = row['price']
-
-                    saleItem.save()
-
-            moveToPath = os.path.join(CSV_PATH, file[3:9])
-            if not os.path.exists(moveToPath):
-                os.makedirs(moveToPath)
-#            handle existing file
-            file_move_safe(filePath,  moveToPath + '/' + file)
-
-    return render(request, 'vanzari.html')
 
 def sales_to_json(request):
 #   magazin,datetime,nrfact,cod,denumire,cant,pret,valoare,categorie
